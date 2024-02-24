232bcfcbc068a96bab252b31ff7135748cebebe9
Syed saim hassan <saimhassan124@gmail.com>
2024-02-24 03:02:39 -0800

    Assignment 02 Js

---
+++ app.js
@@ -0,0 +1,87 @@
+/* Todo-01:Get user to input a number using prompt, check if number is a multiple of 6 or not... */
+
+/* let number = +prompt("Enter value");
+
+
+if(number%6 === 0){
+    document.write(true)
+}
+else{
+
+    document.write(false)
+} */
+
+
+/*==================================================================================================*/
+
+
+
+/* Todo-02:Write a code which can give grades to students according to their marks */
+
+/* let marks = +prompt("Enter your obtained marks under 500");
+let percentage = marks * 100 / 500;
+
+if(percentage>= 80 && percentage<= 100){
+    document.write("Your percentage is " + percentage +"%" + " & Grade A+")
+}
+else if(percentage>= 70 && percentage<= 79){
+    document.write("Your percentage is " + percentage +"%" + " Grade A")
+}
+else if(percentage>= 60 && percentage<= 69){
+    document.write("Your percentage is " + percentage +"%" + " Grade B")
+}
+else if(percentage>= 50 && percentage<= 59){
+    document.write("Your percentage is " + percentage +"%" + " Grade C")
+}
+else if(percentage>= 33 && percentage<= 49){
+    document.write("Your percentage is " + percentage +"%" + " Grade D")
+}
+else if(percentage>= 0 && percentage<= 32){
+    document.write("Your percentage is " + percentage +"%" + " Fail")
+}
+else{
+    document.write("Invalid num")
+} */
+
+
+/*========================================================================================*/
+
+
+/* Todo-03:Create marksheet if else and else if condition */
+
+
+/*
+let studentName = prompt("Enter your name");
+let english = +prompt("English");
+let urdu = +prompt("Urdu");
+let math = +prompt("Mathematics");
+let biology = +prompt("Biology");
+let chemistry = +prompt("Chemistry");
+
+let score = (english + urdu + math + biology + chemistry);
+let grade = (score * 100 / 500);
+if(grade>= 80 && grade<= 100){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Grade A+")
+}
+else if(grade>= 70 && grade<= 79){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Grade A")
+}
+else if(grade>= 60 && grade<= 69){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Grade B")
+}
+else if(grade>= 50 && grade<= 59){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Grade C")
+}
+else if(grade>= 33 && grade<= 49){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Grade D")
+}
+else if(grade>= 0 && grade<= 32){
+    document.write(studentName + " your percentage is " + grade + "%" + " and Fail")
+}
+else{
+    document.write("Invalid result")
+}  */
+
+/*=================================================================================*/
+
+
